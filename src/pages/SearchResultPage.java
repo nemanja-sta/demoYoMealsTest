9915bfe8ae99c15cd8c251e6eea733ea7e2731fb
Nemanja Stanisavljevic <nemanja.sta1@gmail.com>
2020-09-30 16:15:41 +0200

    Searh Test

---
+++ src/pages/SearchResultPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchResultPage extends BasicPage {
+
+	public SearchResultPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor executor) {
+		super(driver, wait, executor);
+	}
+
+	public List<WebElement> searchResults() {
+		return this.driver.findElements(By.xpath("//*[@class='product-name']/a"));
+	}
+
+	public List<String> mealNames() {
+		List<String> mealsFound = new ArrayList<>();
+		for (int i = 0; i < searchResults().size(); i++) {
+			mealsFound.add(searchResults().get(i).getText());
+		}
+		return mealsFound;
+	}
+
+	public int resultNumber() {
+		return this.searchResults().size();
+	}
+}
---
+++ src/tests/SearchTest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.LocationPopupPage;
+import pages.SearchResultPage;
+
+public class SearchTest extends BasicTest {
+
+	@Test(priority = 0)
+	public void searchResultsTest() throws InterruptedException, IOException {
+		SearchResultPage srp = new SearchResultPage(this.driver, this.wait, this.executor);
+		LocationPopupPage lpp = new LocationPopupPage(this.driver, this.wait, this.executor);
+		SoftAssert sa = new SoftAssert();
+
+		this.driver.navigate().to(this.baseUrl + "meals/");
+		lpp.closeDialog();
+		Thread.sleep(2000);
+		lpp.setLocation("City Center - Albany");
+
+		File file = new File("C:\\Users\\Nemanja\\Desktop\\finalProject\\demoYoMealsTest\\data\\Data.xlsx");
+		FileInputStream fis = new FileInputStream(file);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet1 = wb.getSheet("Meal Search Results");
+
+		for (int i = 1; i <= sheet1.getLastRowNum(); i++) {
+		//for (int i = 1; i <= 6; i++) {
+			String newTabScript = "window.open();";
+			executor.executeScript(newTabScript);
+			String mainWindowHandles = driver.getWindowHandle();
+			String newMealWindowHandles = "";
+
+			String location = sheet1.getRow(i).getCell(0).getStringCellValue();
+			String url = sheet1.getRow(i).getCell(1).getStringCellValue();
+			double numberOfResults = sheet1.getRow(i).getCell(2).getNumericCellValue();
+			int numberOfResultsInt = (int) numberOfResults;
+
+			ArrayList<String> wh = new ArrayList<String>(driver.getWindowHandles());
+			wh.remove(mainWindowHandles);
+			newMealWindowHandles = wh.get(0);
+
+			driver.switchTo().window(newMealWindowHandles);
+			driver.navigate().to(url);
+
+			Thread.sleep(2000);
+
+			lpp.setLocation(location);
+
+			Thread.sleep(2000);
+
+			Assert.assertEquals(srp.resultNumber(), numberOfResultsInt, "[ERROR] Meals Number Not Equal");
+
+			for (int j = 3; j < 3 + numberOfResultsInt; j++) {
+				sa.assertTrue(srp.mealNames().get(j - 3).contains(sheet1.getRow(i).getCell(j).getStringCellValue()),
+						"[ERROR] Meals Does Not Present");
+			}
+		}
+
+		sa.assertAll();
+		wb.close();
+		fis.close();
+	}
+
+}
